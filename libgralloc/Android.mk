536e2ee8e7ce402e90d1f2861743890a746394f6
juston <juston@uoregon.edu>
2013-04-01 00:09:37 +0800

    -Wno-attributes for alloc_controller hardware/qcom/display-caf/libgralloc/alloc_controller.cpp:51:269: error: type attributes ignored after type is already defined [-Werror=attributes] cc1plus: all warnings being treated as errors
    Change-Id: I3e930be5d0ab9e6f48650d12abf7dd182dcb3f95

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -39,6 +39,7 @@ LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libdl
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\"
+LOCAL_CFLAGS                  := -Wno-attributes
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := ionalloc.cpp alloc_controller.cpp
 
